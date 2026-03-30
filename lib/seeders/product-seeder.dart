c97b7701472e3f815acdae5242817e03777755d9
Jmgty <joelgutlay04@gmail.com>
2026-03-30 18:51:49 +0800

    add  product seeder

--- lib/seeders/product-seeder.dart
+++ lib/seeders/product-seeder.dart
@@ -8,41 +8,70 @@ import 'package:sqflite/sqflite.dart';
 class ProductSeeder {
   ProductSeeder._();
 
+  static List<Product> _hotCoffees = [
+    Product(
+      id: 1,
+      catId: 1,
+      name: 'Classic Arabica',
+      imagePath: 'assets/images/icedcoffee.jpg',
+      variantLabel: '8oz',
+      price: 45,
+    ),
+    Product(
+      id: 2,
+      catId: 1,
+      name: 'Classic Arabica',
+      imagePath: 'assets/images/hotcoffee.jpg',
+      variantLabel: '16oz',
+      price: 55.0,
+    ),
+    Product(
+      id: 3,
+      catId: 1,
+      name: 'Cafe Latte',
+      imagePath: 'assets/images/icedcoffee.jpg',
+      variantLabel: '8oz',
+      price: 59,
+    ),
+    Product(
+      id: 4,
+      catId: 1,
+      name: 'Cafe Latte',
+      imagePath: 'assets/images/icedcoffee.jpg',
+      variantLabel: '16oz',
+      price: 69,
+    ),
+  ];
+
+  static List<Product> _icedCoffees = [
+    Product(
+      id: 1,
+      catId: 2,
+      name: 'Iced Latte',
+      imagePath: 'assets/images/icedcoffee.jpg',
+      variantLabel: '16oz',
+      price: 39.0,
+    ),
+    Product(
+      id: 2,
+      catId: 2,
+      name: 'Iced Mocha',
+      imagePath: 'assets/images/icedcoffee.jpg',
+      variantLabel: '16oz',
+      price: 49.0,
+    ),
+    Product(
+      id: 2,
+      catId: 2,
+      name: 'Iced Brown Sugar Latte',
+      imagePath: 'assets/images/icedcoffee.jpg',
+      variantLabel: '16oz',
+      price: 49.0,
+    ),
+  ];
+
   static List<Product> _getProducts() {
-    return [
-      Product(
-        id: 1,
-        catId: 1,
-        name: 'Classic Arabica',
-        imagePath: 'assets/images/hotcoffee.jpg',
-        variantLabel: '16oz',
-        price: 55.0,
-      ),
-      Product(
-        id: 2,
-        catId: 1,
-        name: 'Classic Arabica',
-        imagePath: 'assets/images/icedcoffee.jpg',
-        variantLabel: '8oz',
-        price: 45,
-      ),
-      Product(
-        id: 3,
-        catId: 1,
-        name: 'Cafe Latte',
-        imagePath: 'assets/images/icedcoffee.jpg',
-        variantLabel: '8oz',
-        price: 59,
-      ),
-      Product(
-        id: 4,
-        catId: 1,
-        name: 'Cafe Latte',
-        imagePath: 'assets/images/icedcoffee.jpg',
-        variantLabel: '16oz',
-        price: 69,
-      ),
-    ];
+    return [..._hotCoffees, ..._icedCoffees];
   }
 
   static Future<void> seed() async {
