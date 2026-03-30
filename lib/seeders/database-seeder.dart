a9522f5b64334fefb798aa09a222fd7dd728877b
Jmgty <joelgutlay04@gmail.com>
2026-03-30 18:52:27 +0800

    add product seeder

--- lib/seeders/database-seeder.dart
+++ lib/seeders/database-seeder.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:raki_internet_cafe/seeders/category-seeder.dart';
+import 'package:raki_internet_cafe/seeders/product-seeder.dart';
 
 class DatabaseSeeder {
   DatabaseSeeder._();
@@ -8,6 +9,7 @@ class DatabaseSeeder {
   static Future<void> seed() async {
     log('Seeding database...');
     await CategorySeeder.seed();
+    await ProductSeeder.seed();
     log('Database seeded.');
   }
 }
