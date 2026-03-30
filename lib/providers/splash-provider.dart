00f0b04559c61afb25e09d86217d664347450d28
Jmgty <joelgutlay04@gmail.com>
2026-03-30 21:53:34 +0800

    add provider of product

--- lib/providers/splash-provider.dart
+++ lib/providers/splash-provider.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:raki_internet_cafe/helper/db-helper.dart';
 import 'package:raki_internet_cafe/providers/category-provider.dart';
+import 'package:raki_internet_cafe/providers/product-provider.dart';
 import 'package:raki_internet_cafe/repository/category-repository.dart';
+import 'package:raki_internet_cafe/repository/product-repository.dart';
 
 class SplashProvider with ChangeNotifier {
   bool _isLoading = true;
@@ -12,14 +14,22 @@ class SplashProvider with ChangeNotifier {
     DBHelper dbHelper = DBHelper.instance;
     final db = await dbHelper.database;
     final categoryRepo = CategoryRepository(database: db);
+    final productRepo = ProductRepository(database: db);
+
     final categories = await categoryRepo.getAll();
+    final products = await productRepo.getAll();
 
     if (context.mounted) {
       final categoryProvider = Provider.of<CategoryProvider>(
         context,
         listen: false,
       );
+      final productProvider = Provider.of<ProductProvider>(
+        context,
+        listen: false,
+      );
       categoryProvider.setCategories(categories);
+      productProvider.setProducts(products);
     }
 
     await Future.delayed(Duration(seconds: 3));
