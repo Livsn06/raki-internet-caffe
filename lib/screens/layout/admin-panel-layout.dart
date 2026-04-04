7fb110e13181c386cb93f1d13301e3e2c3ba4893
Jmgty <joelgutlay04@gmail.com>
2026-04-04 14:04:59 +0800

    add logout functionality

--- lib/screens/layout/admin-panel-layout.dart
+++ lib/screens/layout/admin-panel-layout.dart
@@ -1,18 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:raki_internet_cafe/core/routing-controls.dart';
+import 'package:raki_internet_cafe/providers/admin-auth-provider.dart';
 import 'package:raki_internet_cafe/screens/admin/admin-panel-screen.dart';
+import 'package:raki_internet_cafe/utils/warrning-modal.dart';
 
 class AdminPanelLayout extends StatelessWidget {
   const AdminPanelLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final authProvider = context.read<AdminAuthProvider>();
+
+    void logout() {
+      warningModal(
+        context,
+        title: "Logout",
+        message: "Are you sure you want to logout?",
+        actions: [
+          MaterialButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            color: Colors.grey,
+            child: const Text("No"),
+          ),
+          MaterialButton(
+            onPressed: () {
+              authProvider.resetProvider();
+              RouteControls.pushAndRemoveUntil(
+                context,
+                RouteScreens.adminAuthScreen,
+              );
+            },
+            color: Colors.red,
+            child: const Text("Yes"),
+          ),
+        ],
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         foregroundColor: Colors.white,
         title: const Text("Admin Panel"),
         centerTitle: true,
         backgroundColor: Colors.black,
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.logout))],
+        actions: [
+          IconButton(onPressed: logout, icon: const Icon(Icons.logout)),
+        ],
       ),
       body: AdminPanelScreen(),
     );
