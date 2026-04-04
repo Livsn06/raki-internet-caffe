afcd8c356708f74d3831217132c2f0a3051395cf
Jmgty <joelgutlay04@gmail.com>
2026-04-04 14:01:57 +0800

    add route

--- lib/core/routing-controls.dart
+++ lib/core/routing-controls.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:raki_internet_cafe/screens/admin/profile-screen.dart';
 import 'package:raki_internet_cafe/screens/layout/admin-auth-layout-screen.dart';
 import 'package:raki_internet_cafe/screens/layout/admin-panel-layout.dart';
+import 'package:raki_internet_cafe/screens/layout/profile-layout-screen.dart';
 
 import '../screens/layout/product-layout-screen.dart';
 
@@ -38,5 +40,6 @@ class RouteScreens {
   RouteScreens._();
   static const productScreen = ProductLayoutScreen();
   static const adminAuthScreen = AdminAuthLayoutScreen();
-  static const adminPanel = AdminPanelLayout();
+  static const adminPanelScreen = AdminPanelLayout();
+  static const profileScreen = ProfileLayoutScreen();
 }
