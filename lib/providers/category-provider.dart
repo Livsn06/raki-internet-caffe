3d7f1cab9316de0b03946c6a5e1ca34b96816998
Jmgty <joelgutlay04@gmail.com>
2026-04-09 18:49:44 +0800

    add refresh data

--- lib/providers/category-provider.dart
+++ lib/providers/category-provider.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:raki_internet_cafe/helper/db-helper.dart';
 import 'package:raki_internet_cafe/models/category-model.dart';
+import 'package:raki_internet_cafe/repository/category-repository.dart';
 
 class CategoryProvider extends ChangeNotifier {
   int _selectedCategoryId = 1;
@@ -16,6 +18,14 @@ class CategoryProvider extends ChangeNotifier {
 
   void setSelectedCategory(int catId) {
     _selectedCategoryId = catId;
+    notifyListeners();
+  }
+
+  Future<void> refresh() async {
+    final database = await DBHelper.instance.database;
+    final repo = CategoryRepository(database: database);
+    // await Future.delayed(Duration(seconds: 5));
+    _categories = await repo.getAll();
     notifyListeners();
   }
 }
